242a4f5c2bc505fede572fa7f6e7bd81f1ebb13e
Quim <joaquimpujol@gmail.com>
2023-01-29 23:14:53 +0100

    START initial commit - structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaisModule } from "./pais/pais.module";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        PaisModule,
+        SharedModule
+    ]
 })
 export class AppModule { }
